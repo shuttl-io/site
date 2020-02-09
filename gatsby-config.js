0087e4e9c5dc94ebfe6220dfebaef3fa19f7d6c0
Yoseph Radding <yoseph@shuttl.io>
2020-02-08 19:39:11 -0500

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,26 @@ module.exports = module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-83670049-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
+    {
       resolve: `gatsby-source-wordpress`,
       options: {
         baseUrl: `content-shuttl.herokuapp.com`,
